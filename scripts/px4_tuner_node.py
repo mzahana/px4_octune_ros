1c66a9761c083f3f0fbab6c6cc48dbab0729303f
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2021-06-27 22:39:00 +0300

    Add new script px4_tuner_node.py

---
+++ scripts/px4_tuner_node.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import rospy
+from octune.optimization import BackProbOptimizer
+
+class PX4Tuner:
+    def __init__(self):
+        pass
+        
+def main():
+    pass
+
+
