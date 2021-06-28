d4f5febaa881ff3af37aeb5825e3e7eb05e6e13e
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2021-06-28 20:34:27 +0300

    Update px4_tuner_node.py

--- scripts/px4_tuner_node.py
+++ scripts/px4_tuner_node.py
@@ -1,13 +1,222 @@
 #!/usr/bin/env python
 
 import rospy
+from mavros_msgs.msg import AttitudeTarget, ActuatorControl, PositionTarget
+from mavros_msgs.srv import MessageInterval, MessageIntervalRequest, MessageIntervalResponse
+from sensor_msgs.msg import Imu
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import TwistStamped
+from math import ceil
 from octune.optimization import BackProbOptimizer
 
 class PX4Tuner:
     def __init__(self):
+        # Print debug messages
+        self._debug = rospy.get_param('~debug', False)
+
+        # Commanded attitude rates, dictionary, has two arrays, timesatmp and commanded attitude rates
+        self._cmd_att_rate_dict=        {'time':[], 'x':[], 'y':[], 'z':[]}
+        # Commanded attitude rates, dictionary, has two arrays, timesatmp and commanded attitude
+        self._cmd_att_dict=             {'time':[], 'x':[], 'y':[], 'z':[]}
+        # Commanded velocity, dictionary, has two arrays, timesatmp and commanded velocity
+        self._cmd_vel_dict=             {'time':[], 'x':[], 'y':[], 'z':[]}
+        # Commanded position, dictionary, has two arrays, timesatmp and commanded position
+        self._cmd_pos_dict=             {'time':[], 'x':[], 'y':[], 'z':[]}
+
+        # Feedback, attitude rates, dictionary, two arrays, time & data
+        self._att_rate_dicte=           {'time':[], 'x':[], 'y':[], 'z':[]}
+        # Feedback, attitude,  dictionary, two arrays, time & data
+        self._att_dict=                 {'time':[], 'x':[], 'y':[], 'z':[]}
+        # Feedback, local velocity,  dictionary, two arrays, time & data
+        self._vel_dict=                 {'time':[], 'x':[], 'y':[], 'z':[]}
+        # Feedback, local position,  dictionary, two arrays, time & data
+        self._pos_dict=                 {'time':[], 'x':[], 'y':[], 'z':[]}
+
+        # System excitation time, seconds
+        self._excitation_t = rospy.get_param('~excitation_t', 2.0)
+
+        # Sampling/upsampling time, seconds
+        self._sampling_dt = rospy.get_param('~sampling_dt', 0.01)
+
+        # Maximum array length, maybe dependent on excitiation time and sampling time?
+        self._max_arr_L=int(ceil(self._excitation_t/self._sampling_dt))
+
+        # Flag to record/store data
+        self._record_data=False
+
+
+
+        # Commanded attitude
+        rospy.Subscriber("mavros/setpoint_raw/target_attitude",AttitudeTarget, self.cmdAttCb)
+        # Commanded angular rates
+        rospy.Subscriber("mavros/target_actuator_control",ActuatorControl, self.cmdRatesCb)
+        # Commanded position/velocity/acceleration
+        rospy.Subscriber("mavros/setpoint_raw/target_local", PositionTarget, self.cmdPosCb)
+
+        # Feedback, angular velocity and linear acceleration
+        rospy.Subscriber("mavros/imu/data_raw", Imu, self.rawImuCb)
+        # Feedback, angular velocity and attitude
+        rospy.Subscriber("mavros/imu/data", Imu, self.imuCb)
+        # Feedback, local pose
+        rospy.Subscriber("mavros/local_position/pose", PoseStamped, self.poseCb)
+        # Feedback, local velocity
+        rospy.Subscriber("mavros/local_position/velocity_local", TwistStamped, self.velCb)
+
+    def insertData(self, dict=None, t=None, x=None, y=None, z=None):
+        """Inserts a data point into a dictionary dict
+
+        @param dict Input dictionary with two arrays/lists, time and data
+        @param t Timestamp in micro seconds
+        @param x Data point in x axis
+        @param y Data point in y axis
+        @param z Data point in z axis
+
+        @return out_dict Populated dictionary after data point insertion
+        """
+        if t is None or x is None or y is None or z is None or dict is None:
+            rospy.logerr("[insertData] One of the input arguments is None")
+            return
+        # TODO Make sure that the input dict is with the right data struction, time & data arrays
+
+        out_dict=dict
+        out_dict['time'].append(t)
+        out_dict['x'].append(x)
+        out_dict['y'].append(y)
+        out_dict['z'].append(z)
+        if len(out_dict['time']) > self._max_arr_L:
+            out_dict['time'].pop(0)
+        if len(out_dict['x']) > self._max_arr_L:
+            out_dict['x'].pop(0)
+        if len(out_dict['y']) > self._max_arr_L:
+            out_dict['y'].pop(0)
+        if len(out_dict['z']) > self._max_arr_L:
+            out_dict['z'].pop(0)
+
+        return out_dict
+
+    def resetDict(self):
+        """Clears all data dictionaries
+        """
+        self._cmd_att_rate_dict=        {'time':[], 'x':[], 'y':[], 'z':[]}
+        self._cmd_att_dict=             {'time':[], 'x':[], 'y':[], 'z':[]}
+        self._cmd_vel_dict=             {'time':[], 'x':[], 'y':[], 'z':[]}
+        self._cmd_pos_dict=             {'time':[], 'x':[], 'y':[], 'z':[]}
+        self._att_rate_dicte=           {'time':[], 'x':[], 'y':[], 'z':[]}
+        self._att_dict=                 {'time':[], 'x':[], 'y':[], 'z':[]}
+        self._vel_dict=                 {'time':[], 'x':[], 'y':[], 'z':[]}
+        self._pos_dict=                 {'time':[], 'x':[], 'y':[], 'z':[]}
+
+    def increaseStreamRates(self):
+        """Requests from PX4 an increase in stream rates of commanded/feedback signals
+        """
+        ACTUATOR_CONTROL_TARGET=140
+        ATTITUDE_TARGET=83
+        POSITION_TARGET_LOCAL_NED=85
+        HIGHRES_IMU=105
+        ATTITUDE_QUATERNION=31
+        LOCAL_POSITION_NED=32
+        # TODO Implement rosservice call of /mavros/set_message_interval
         pass
+
+    
+    def cmdAttCb(self, msg):
+        if msg is None:
+            return
+        
+        t = rospy.Time(secs=msg.header.stamp.secs, nsecs=msg.header.stamp.nsecs)
+        t_micro = t.to_nsec()/1000
+            
+        
+
+    def cmdRatesCb(self, msg):
+        if msg is None:
+            return
+
+        t = rospy.Time(secs=msg.header.stamp.secs, nsecs=msg.header.stamp.nsecs)
+        t_micro = t.to_nsec()/1000
+        x = msg.controls[0]
+        y = msg.controls[1]
+        z = msg.controls[2]
+
+        if self._record_data:
+            self._cmd_att_rate_dict = self.insertData(dict=self._cmd_att_rate_dict, t=t, x=x, y=y, z=z)
+            if self._debug:
+                rospy.loginfo_throttle("cmd_att_rate_dict length is: %s",len(self._cmd_att_rate_dict['time']))
+
+    def apllyStepInput(self):
+        """Sends local velocity step inputs to excite the system
+        """
+        # TODO implement step inputs; required mavros offboard controller
+
+    def startRecordingData(self):
+        """Sets _record_data=True so that the callbacks starts storing data in the dictionaries
+        """
+        self._record_data=True
+
+    def stopsRecordingData(self):
+        """Sets _record_data=False so that the callbacks stop storing data in the dictionaries
+        """
+        self._record_data=False
+
+    def preProcessData(self):
+        """Organizes data, aligns their timesamps, and upsamples them, in preparation for optimization
+        """
+        # TODO Implement data pre-processing
+        pass
+
+    def tuneRatePID(self):
+        """Tunes the angular rates PID loops
+        """
+        # TODO 
+        # reset data dictionaries
+        # get current PID gains
+        # apply step inputs
+        # record data
+        # optimize
+        # Check new PID gains
+        # if gains are in acceptable range, send new gains to FCU
+        # repeat until performance is acceptable (what is the metric?)
+        pass
+
+    def tuneVelPIDs(self):
+        """Tunes linear velocity PID loops
+        """
+        # TODO 
+        pass
+
+    def cmdPosCb(self, msg):
+        if msg is None:
+            return
+        # TODO
+
+    def rawImuCb(self, msg):
+        if msg is None:
+            return
+        # TODO
+
+    def imuCb(self, msg):
+        if msg is None:
+            return
+        # TODO
+    
+    def poseCb(self, msg):
+        if msg is None:
+            return
+        # TODO
+
+    def velCb(self, msg):
+        if msg is None:
+            return
+        # TODO
+        
         
 def main():
-    pass
+    rospy.init_node("px4_tuner_node", anonymous=True)
+    rospy.loginfo("Starting px4_tuner_node...\n")
+    obj = PX4Tuner()
+    obj._record_data=True
+    obj._debug=True
+    rospy.spin()
 
-
+if __name__ == "__main__":
+    main()
