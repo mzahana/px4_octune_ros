4b22b3468f218e810a713bfe3dbcf87403d7f8d0
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2021-07-04 17:30:49 +0300

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,7 +7,18 @@
 echo "Installing px4_octune_ros dependencies ..." && echo
 sleep 1
 
-sudo apt-get update
+if [ -z ${GIT_TOKEN} ]; then
+    CLONE_OCTUNE="git clone https://github.com/mzahana/octune.git"
+else
+    CLONE_OCTUNE="git clone https://${GIT_TOKEN}@github.com/mzahana/octune.git"
+fi
+
+if [-z ${SUDO_PASS} ]; then
+    sudo apt-get update
+else
+    echo $SUDO_PASS | sudo -S apt-get update
+fi
+
 
 # We need a src folder in the HOMe folder
 if [ ! -d "$HOME/src" ]; then
@@ -20,7 +31,7 @@ cd $HOME/src
 # Clone OCTUNE package
 if [ ! -d "$HOME/src/octune" ]; then
     cd $HOME/src
-    git clone https://github.com/mzahana/octune.git
+    $CLONE_OCTUNE
 else
     echo "octune package already exists in $HOME/src/octune. Pulling latest updates..." && echo
     cd $HOME/src/octune
@@ -36,7 +47,16 @@ cd $HOME/src/octune && git checkout python2
 #sudo apt install python3-pip -y
 #sudo apt install python-pip -y
 
-sudo pip uninstall -y numpy
+# This is to solve installation issues related to pip (Python 2.7)
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
+python get-pip.py --force-reinstall
+
+if [-z ${SUDO_PASS} ]; then
+    sudo pip uninstall -y numpy
+else
+    echo $SUDO_PASS | sudo -S pip uninstall -y numpy
+fi
+
 pip install numpy
 pip install matplotlib --user
 pip install scipy --user
